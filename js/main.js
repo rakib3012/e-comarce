743b9eeb95e217e4e947dc50c367c0430a7317ed
Rakib <rakibuzzaman1177@gmail.com>
2023-01-26 06:58:11 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+const bar=document.getElementById('bar');
+const close=document.getElementById('close');
+const nav=document.getElementById('nav');
+
+if (bar) {
+    bar.addEventListener('click',  function () {
+            nav.classList.add('active');
+        })
+}
+
+
+if (close) {
+    close.addEventListener('click',  function () {
+            nav.classList.remove('active');
+        })
+}
